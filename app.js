a41e9d9a22582a3eedc7002b08725a38478ef09e
m4rt1nq <dgciid@gmail.com>
2014-07-23 13:24:17 -0300

    commit message

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express"),
     app     = express(),
     http    = require("http"),
     server  = http.createServer(app);
+    mongoose= require('mongoose');
 
 app.configure(function () {
   app.use(express.bodyParser());
@@ -13,6 +14,21 @@ app.get('/', function(req, res) {
   res.send("Hello world!");
 });
 
+//connect to file and db api REST CRUD tvshows
+routes = require('./routes/tvshows')(app);
+
+
+//connect to mongoose
+mongoose.connect('mongodb://localhost/tvshows', function(err,res) {
+  if(err) {
+    console.log('ERROR: Connecting to Database. '+err);
+  } else {
+    console.log('Connected to Databse');
+  }
+});
+
+
 server.listen(3000, function() {
   console.log("Node server running on http://localhost:3000");
 });
+
